4be43c83ab3e1e12d5b1a96e3e3d50aea37f601c
OverMg <alejitasoto111@gmail.com>
2024-10-26 15:07:22 -0500

    comentarios

--- configs.js
+++ configs.js
@@ -4,17 +4,17 @@ module.exports = {
         sucess: '#FF6BB1',
         error: 'Red'
     },
-    ownerID: "", // Your ID
+    ownerID: "245339452464037888", // id del propietario
     channeldErrors: {
-        context: "", // LOG ERROR IN COMMANDS CONTEXT
-        slash: "", // LOG ERROR IN COMMANDS SLASH
-        prefix: "" // LOG ERROR IN COMMANDS PREFIXES
+        context: "1287505300768690227", // errores en comandos de contexto
+        slash: "1287492491708334193", // errores en comandos de slash
+        prefix: "1287499469939802143" // errores en comandos de prefix
     },
-    devs: [""], // Add your devs IDs
+    devs: ["245339452464037888"], // devs IDs
     logs: {
-        errorsChannelID: "CHANNEL ID TO REGISTRY ERRORS",
-        slashChannelID: "CHANNEL ID TO REGISTRY SLASH USES",
-        prefixChannelID: "CHANNEL ID TO REGISTRY PREFIXES USES",
-        shardLogsURL: "WEBHOOK URL",
+        errorsChannelID: "1287505300768690227", // canal de registro de errores
+        slashChannelID: "1287492491708334193", // canal de registro del uso de comandos de slash
+        prefixChannelID: "1287499469939802143", // canal de registro del uso de comandos de prefijo
+        shardLogsURL: "" // URL del webhook de registro de shards
     }
 }
